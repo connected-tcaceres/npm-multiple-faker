99dde81667580cf532714dbb4baca22107596ad7
tylerccaceres <tyler.caceres@gmail.com>
2019-10-06 17:11:53 -0400

    attempt to create error testing

--- dist/index.js
+++ dist/index.js
@@ -2,18 +2,33 @@ const faker = require("faker");
 
 const rows = 10;
 const attributes = {
-  name: ["firstName", "lastName", "jobTitle"],
+  name: ["firstName", "lastName", "jobTitle", "poop"],
   phone: ["phoneNumber"],
   random: ["number"]
 };
 
-for (let i = 0; i < rows; i++) {
-  console.log(`ROW NUMBER : ${i}`);
-  for (const attr in attributes) {
-    console.log(`ATTRIBUTE : ${attr}`);
-    for (const subAttr of attributes[attr]) {
-      console.log(`SUB-ATTRIBUTE : ${subAttr}`);
-      console.log(`VALUE : ${faker[attr][subAttr]()}`);
+console.log(faker.name());
+
+for (const attr in attributes) {
+  if (!faker[attr]) {
+    throw new Error(`Faker attribute of "${attr}" does not exist.`);
+  }
+  for (const subAttr of attributes[attr]) {
+    if (!faker[attr][subAttr]) {
+      throw new Error(`Faker attribute ${attr}'s sub-attribute of "${subAttr}" does not exist.`);
     }
   }
 }
+
+// const data = {};
+// for (let i = 0; i < rows; i++) {
+//   for (const attr in attributes) {
+//     if (!faker[attr]) {
+//       throw new Error(`Faker attribute of "${attr}" does not exist.`);
+//     }
+//     for (const subAttr of attributes[attr]) {
+//       console.log(`SUB-ATTRIBUTE : ${subAttr}`);
+//       console.log(`VALUE : ${faker[attr][subAttr]()}`);
+//     }
+//   }
+// }
